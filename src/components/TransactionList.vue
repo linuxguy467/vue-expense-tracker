67b18536dc495f8121423d311a60dab005f9ac27
linuxguy467 <me@matthem.codes>
2023-11-19 19:27:59 -0500

    add transaction list with options api

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,7 +1,24 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">Cash <span>-$400</span><button class="delete-btn">x</button></li>
-    <li class="plus">Paycheck <span>$800</span><button class="delete-btn">x</button></li>
+    <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
   </ul>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: 'Paycheck', amount: 221.99 },
+        { id: 2, text: 'Sell Car', amount: 114.67 },
+        { id: 3, text: 'Stereo set', amount: -604 },
+        { id: 4, text: 'Book', amount: -16 },
+      ],
+    }
+  },
+}
+</script>
