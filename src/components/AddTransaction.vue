5e434bb46b1b5181e77edc4372a8607593e04224
linuxguy467 <me@matthem.codes>
2023-11-20 14:16:55 -0500

    add input sanitization in the form

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -3,14 +3,14 @@
   <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="text">Text</label>
-      <input type="text" id="text" v-model="text" placeholder="Enter text..." />
+      <input type="text" id="text" v-model="text" placeholder="Enter text..." @blur="sanitizeInput" />
     </div>
     <div class="form-control">
       <label for="amount">
         Amount <br />
         (negative - expense, positive - income)
       </label>
-      <input type="number" step="0.01" v-model="amount" id="amount" placeholder="Enter amount..." />
+      <input type="number" step="0.01" v-model="amount" id="amount" placeholder="Enter amount..." @blur="sanitizeInput" />
     </div>
     <button class="btn">Add transaction</button>
   </form>
@@ -20,12 +20,18 @@
 import { ref } from 'vue'
 import { useToast } from 'vue-toastification'
 
+import DOMPurify from 'dompurify'
+
 const text = ref('')
 const amount = ref(0.0)
 
 const emit = defineEmits(['transactionSubmitted'])
 
 const toast = useToast()
+
+const sanitizeInput = (event) => {
+  text.value = DOMPurify.sanitize(event.target.value)
+}
 
 const onSubmit = () => {
   if (!text.value || !amount.value) {
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -2,8 +2,9 @@
   <h3>History</h3>
   <ul id="list" class="list">
     <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
-      {{ transaction.text }} <span>${{ transaction.amount }}</span
-      ><button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
+      <div v-html="transaction.text"></div>
+      <span>${{ transaction.amount }}</span>
+      <button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
     </li>
   </ul>
 </template>
