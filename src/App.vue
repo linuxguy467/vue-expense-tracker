cf76f878f1344fa024563650d19987a2c05a9ea8
linuxguy467 <me@matthem.codes>
2023-11-19 20:38:26 -0500

    compute balance

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Header />
   <div class="container">
-    <Balance />
+    <Balance :total="total" />
     <IncomeExpenses />
     <TransactionList :transactions="transactions" />
     <AddTransaction />
@@ -15,12 +15,20 @@ import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue'
 import AddTransaction from './components/AddTransaction.vue'
 
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const transactions = ref([
   { id: 1, text: 'Paycheck', amount: 221.99 },
   { id: 2, text: 'Sell Car', amount: 114.67 },
   { id: 3, text: 'Stereo set', amount: -604 },
-  { id: 4, text: 'Book', amount: -16 },
+  { id: 4, text: 'Book', amount: 16 },
 ])
+
+const total = computed(() => {
+  return parseFloat(
+    transactions.value.reduce((acc, { amount }) => {
+      return acc + amount
+    }, 0)
+  ).toFixed(2)
+})
 </script>
